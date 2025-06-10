37f324b1f5e5ae6472796a8f477432856ab24201
jppluas <jppluas@espol.edu.ec>
2025-06-10 12:32:54 -0500

    FIX build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: 'dist', // Especifica la carpeta donde se generará el build
+    sourcemap: true, // Esto puede ayudarte a depurar el código generado
+  },
+});
